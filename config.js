ddefba179597f326cd995d9250d67e749ef78f2f
15299967148@163.com <15299967148@163.com>
2026-02-16 11:46:32 +0800

    Commit

---
+++ config.js
@@ -0,0 +1,14 @@
+/*
+    注意：请直接修改source或ihdr的值，即'='后面的 "" 内的路径
+    source来源于根目录下的/assets/audio/
+    idhr来源于根目录下的/assets/texture/
+    source = "gs.mp3"
+    
+    *source表示要播放的音频的名称，该音频必须位于/assets/audio/下
+    *ihdr表示全景天空盒，该图片必须位于必须位于/assets/audio/下
+    
+    *你可以添加合适的资源于以上2个目录
+*/
+
+source = "va.mp3"
+ihdr = "city.jpg"
